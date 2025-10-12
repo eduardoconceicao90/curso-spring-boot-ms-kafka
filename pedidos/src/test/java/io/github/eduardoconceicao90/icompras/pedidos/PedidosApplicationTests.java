a1bc121cc2fae3f014d63572da892762edc61ed7
Eduardo Conceição <eduardosaconceicao@gmail.com>
2025-10-12 07:45:00 -0300

    Criação e configuração do microservice de Pedidos

---
+++ pedidos/src/test/java/io/github/eduardoconceicao90/icompras/pedidos/PedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.eduardoconceicao90.icompras.pedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
