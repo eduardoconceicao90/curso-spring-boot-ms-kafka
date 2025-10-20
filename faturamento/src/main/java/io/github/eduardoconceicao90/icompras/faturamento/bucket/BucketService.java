75ed0364ec1f8484448611b4e878a2c093f0f365
Eduardo Conceição <eduardosaconceicao@gmail.com>
2025-10-20 18:56:04 -0300

    Fazendo upload para o bucket

--- faturamento/src/main/java/io/github/eduardoconceicao90/icompras/faturamento/bucket/BucketService.java
+++ faturamento/src/main/java/io/github/eduardoconceicao90/icompras/faturamento/bucket/BucketService.java
@@ -1,9 +1,14 @@
 package io.github.eduardoconceicao90.icompras.faturamento.bucket;
 
 import io.github.eduardoconceicao90.icompras.faturamento.config.props.MinioProps;
+import io.minio.GetPresignedObjectUrlArgs;
 import io.minio.MinioClient;
+import io.minio.PutObjectArgs;
+import io.minio.http.Method;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
 
 @RequiredArgsConstructor
 @Service
@@ -13,11 +18,33 @@ public class BucketService {
     private final MinioProps props;
 
     public void upload(BucketFile file){
+        try {
+            var object = PutObjectArgs.builder()
+                    .bucket(props.getBucketName())
+                    .object(file.name())
+                    .stream(file.inputStream(), file.size(), -1)
+                    .contentType(file.mediaType().toString())
+                .build();
 
+            client.putObject(object);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public String getUrl(String fileName){
-        return "";
+        try {
+            var objectUrl = GetPresignedObjectUrlArgs.builder()
+                        .method(Method.GET)
+                        .bucket(props.getBucketName())
+                        .object(fileName)
+                        .expiry(1, TimeUnit.HOURS)
+                    .build();
+
+            return client.getPresignedObjectUrl(objectUrl);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
---
+++ faturamento/src/main/java/io/github/eduardoconceicao90/icompras/faturamento/controller/BucketController.java
@@ -0,0 +1,37 @@
+package io.github.eduardoconceicao90.icompras.faturamento.controller;
+
+import io.github.eduardoconceicao90.icompras.faturamento.bucket.BucketFile;
+import io.github.eduardoconceicao90.icompras.faturamento.bucket.BucketService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.InputStream;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("bucket")
+public class BucketController {
+
+    private final BucketService service;
+
+    @PostMapping
+    public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file) {
+        try (InputStream inputStream = file.getInputStream()) {
+
+            MediaType mediaType = MediaType.parseMediaType(file.getContentType());
+            var bucketFile = new BucketFile(file.getOriginalFilename(), inputStream, mediaType, file.getSize());
+            service.upload(bucketFile);
+
+            return ResponseEntity.ok("Arquivo enviado com sucesso!");
+
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body("Erro ao fazer upload do arquivo: " + e.getMessage());
+        }
+    }
+}
