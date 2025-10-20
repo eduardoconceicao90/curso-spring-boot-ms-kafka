fe93eb7282662abc3e6f41d3039f6f40866e9875
Eduardo Conceição <eduardosaconceicao@gmail.com>
2025-10-20 19:51:16 -0300

    Acessando arquivos do bucket

--- faturamento/src/main/java/io/github/eduardoconceicao90/icompras/faturamento/controller/BucketController.java
+++ faturamento/src/main/java/io/github/eduardoconceicao90/icompras/faturamento/controller/BucketController.java
@@ -5,10 +5,7 @@ import io.github.eduardoconceicao90.icompras.faturamento.bucket.BucketService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.InputStream;
@@ -34,4 +31,15 @@ public class BucketController {
             return ResponseEntity.status(500).body("Erro ao fazer upload do arquivo: " + e.getMessage());
         }
     }
+
+    @GetMapping
+    public ResponseEntity<String> getUrl(@RequestParam("filename") String filename) {
+        try {
+            String url = service.getUrl(filename);
+            return ResponseEntity.ok(url);
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body("Erro ao obter URL do arquivo: " + e.getMessage());
+        }
+    }
+
 }
