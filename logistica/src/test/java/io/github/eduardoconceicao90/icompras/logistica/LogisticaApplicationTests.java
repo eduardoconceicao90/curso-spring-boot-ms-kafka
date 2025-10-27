9feeba687a8e7d8965b13fcba6d1f256af81bfd5
Eduardo Conceição <eduardosaconceicao@gmail.com>
2025-10-27 11:13:20 -0300

    Criando microservice de logistica

---
+++ logistica/src/test/java/io/github/eduardoconceicao90/icompras/logistica/LogisticaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.eduardoconceicao90.icompras.logistica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
